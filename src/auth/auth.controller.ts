39154a64363fa423b8719ded71bc08cd738ed7fa
hasharas <hasharamsankalpam@gmail.com>
2025-03-30 00:15:14 +0530

    Use:dto file use

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto';
+
 
 @Controller('auth')
 export class AuthController {
-      constructor(private authService:AuthService){ }
+      constructor(private authService: AuthService) { }
 
       @Post('signup')
-      signup(@Body() dto:AuthDto){
-           console.log({
-               dto
-            })
+      signup(@Body('email') email: string,
+            @Body('password') password: string,) {
+            console.log({
+                  email,
+                  password,
+            });
             return this.authService.signup();
       }
 
       @Post('signin')
-      signin(){
-            return this.authService.signin() ;
+      signin() {
+            return this.authService.signin();
       }
 }
- 
